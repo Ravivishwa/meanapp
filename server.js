77ccd8d65bbff5888bf4c390b0e3a421668cb33f
Ravivishwa <raviv.vishwakarma@gmail.com>
2017-10-24 20:42:14 +0530

    started

---
+++ server.js
@@ -0,0 +1,21 @@
+var express		=require('express');
+var app			=express();
+var port        =process.env.PORT || 8080;
+var morgan      =require('morgan');
+var mongoose	=require('mongoose');
+var User		=require('./app/models/user');
+
+app.use(morgan('dev'));
+
+
+mongoose.connect('mongodb://localhost:27017/tutorial',function(err){
+	if(err){
+		console.log('not connected' + err);
+	}
+	else{
+		console.log('connected');
+	}
+});
+app.listen(port,function(){
+	console.log('runing server' + port);
+});
