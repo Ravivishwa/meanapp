49c9b16e6d483cf8d7557ca9db89dfa1432dedfc
Ravikant Vishwakarma <raviv.vishwakarma@gmail.com>
2022-09-14 15:08:17 +0530

    test

--- app/models/user.js
+++ app/models/user.js
@@ -5,7 +5,7 @@ var UserSchema=new Schema({
   username:{type:String,lowercase:true,required:true,unique:true},
   password:{type:String,required:true},
   email:{type:String,required:true,lowercase:true,unique:true}
-});
+})
 module.exports=mongoose.model('User',UserSchema);
 
 
